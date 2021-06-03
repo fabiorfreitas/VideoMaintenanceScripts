03b1fa1850a2aef7d51ac0260d720495fadc59fc
fabiorzfreitas <mckfabio@gmail.com>
2021-06-02 23:16:35 -0300

    Unificado
    O script atual executa da maneira mais concisa o que os 3 anteriores faziam. Falta apenas adicionar indicativos do processo ("echo")

--- 1-alltomkv.cmd
+++
@@ -1,6 +0,0 @@
-@echo off
-for /r %%a in (*.mp4 *.avi *.mov) do (
-    mkvmerge -o "%%~pna.mkv" -S -M -T --no-global-tags --no-chapters "%%~a"
-	del "%%~a"
-)
-cmd /k
--- VideoFilesScan.cmd
+++ VideoFilesScan.cmd
@@ -1,6 +1,7 @@
 @echo off
+setlocal EnableDelayedExpansion
 for /r %%a in (*.mkv *.mp4 *.avi *.mov) do (
-    if /i not %%~xa==mkv do (
+    if /i not %%~xa==mkv (
         mkvmerge -o "%%~pna.mkv" -S -M -T --no-global-tags --no-chapters "%%~a"
 	    if errorlevel 1 (
             echo Warnings/errors generated during remuxing, original file not deleted
@@ -13,7 +14,15 @@ for /r %%a in (*.mkv *.mp4 *.avi *.mov) do (
         )
         for /f %%c in ('echo !info! ^| find /c /i "subtitles"') do (
             if [%%c]==[0] (
-                echo "search for extras"
+                for /f %%c in ('mkvmerge -i "%%g" ^| findstr /R /C:"[0-9s]: [0-9]" /C:"bytes" ^| find /c /v ""') do (
+			        if [%%c]==[0] (
+			            echo "%%g" has no extras
+				    ) else (
+				        echo "%%g"
+					    mkvpropedit "%%~fg" --tags all: --chapters ""
+					)
+		    )
+
             ) else (
                 mkvmerge -o "%%~dpna.nosubs%%~xa" -S -M -T --no-global-tags --no-chapters "%%a"
                 if errorlevel 1 (
@@ -22,7 +31,7 @@ for /r %%a in (*.mkv *.mp4 *.avi *.mov) do (
                     del /f "%%a"
 				    ren "%%~dpna.nosubs%%~xa" "%%~nxa"
                 )
-            echo.
+                echo.
             )
 
         )
