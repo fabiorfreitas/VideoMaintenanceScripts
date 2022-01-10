59f82135a49cd0ce0fb618e498334c64872339d6
fabiorzfreitas <mckfabio@gmail.com>
2022-01-10 19:13:24 -0300

    .exe
    Added .exe to external executables to improve speed.

--- VideoFilesScan.cmd
+++ VideoFilesScan.cmd
@@ -5,33 +5,33 @@ for /r %%a in (*.mkv *.mp4 *.avi *.mov) do (
     echo ###
     echo Processing "%%a"
     if /i not "%%~xa" == ".mkv" (
-        mkvmerge -o "%%~pna.mkv" -S -M -T -B --no-global-tags --no-chapters --ui-language en "%%~a"
+        mkvmerge.exe -o "%%~pna.mkv" -S -M -T -B --no-global-tags --no-chapters --ui-language en "%%~a"
         if errorlevel 1 (
             echo Warnings/errors generated during remuxing, original file not deleted
-            mkvmerge -i --ui-language en "%%a">>errors.txt
+            mkvmerge.exe -i --ui-language en "%%a">>errors.txt
             del "%%~pna.mkv"
         ) else (
             echo Deleting old file
             del "%%~a"
         )
     ) else (
-        call :mkvmergeinfoloop "%%a"
+        call :mkvmerge.exeinfoloop "%%a"
     )
 )
 cmd /k
 
-:mkvmergeinfoloop
+:mkvmerge.exeinfoloop
 setlocal EnableExtensions DisableDelayedExpansion
-for /f "delims=" %%l in ('mkvmerge -i "%~1" --ui-language en') do (
+for /f "delims=" %%l in ('mkvmerge.exe -i "%~1" --ui-language en') do (
     for /f "tokens=1,4 delims=:( " %%t in ("%%l") do (
         if /i "%%u" == "subtitles" (
             echo ###
             echo "%~1" has subtitles
-            mkvmerge -o "%~dpn1.nosubs%~x1" -S -M -T -B --no-global-tags --no-chapters --ui-language en "%~1"
+            mkvmerge.exe -o "%~dpn1.nosubs%~x1" -S -M -T -B --no-global-tags --no-chapters --ui-language en "%~1"
             if errorlevel 1 (
                 echo ###
                 echo Warnings/errors generated during remuxing, original file not deleted, check errors.txt
-                mkvmerge -i --ui-language en "%~1">>errors.txt
+                mkvmerge.exe -i --ui-language en "%~1">>errors.txt
                 del "%~dpn1.nosubs%~x1"
             ) else (
 				echo Deleting old file
@@ -48,9 +48,9 @@ for /f "delims=" %%l in ('mkvmerge -i "%~1" --ui-language en') do (
     if defined propedit (
         echo ###
         echo "%~1" has extras
-        mkvpropedit "%~f1" --delete-attachment mime-type:image/jpeg --chapters "" --tags all:
-        mkvpropedit "%~f1" --delete-attachment mime-type:application/x-truetype-font
-        mkvpropedit "%~f1" --delete-attachment mime-type:application/vnd.ms-opentype
+        mkvpropedit.exe "%~f1" --delete-attachment mime-type:image/jpeg --chapters "" --tags all:
+        mkvpropedit.exe "%~f1" --delete-attachment mime-type:application/x-truetype-font
+        mkvpropedit.exe "%~f1" --delete-attachment mime-type:application/vnd.ms-opentype
         goto :eof       
     )
 )
